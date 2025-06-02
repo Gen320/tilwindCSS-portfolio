48428a50ef6921cc4fdfa04946e1d2f583e94e5f
Gen <ssatou0320@gmail.com>
2025-06-02 07:06:52 +0000

    tochuudayo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,27 @@
 //ナビゲーションバー
 
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
 export const Header: React.FC = () => {
+  const [isHidden, setIshidden] = useState(false);
+
+  const handleHidden = () => {
+    setIshidden((prev) => !prev);
+  };
   return (
     <header className="py-6">
       <div className="container mx-auto flex justify-between items-center px-8 md:px-14">
         <div className="text-3xl font-bold"> Gen.com </div>
-        <div className="space-x-12">
+        <button className="md:hidden" onClick={handleHidden}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <div
+          className={`space-x-12 ${
+            isHidden && "hidden"
+          } md:flex md:flex-col md:items-right`}
+        >
           <a
             href="/"
             className="hover:text-selected-text transition-all duration-300"
