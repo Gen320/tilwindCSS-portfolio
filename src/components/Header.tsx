2b16938aca617f2bce2418bea177c1a14cea81a4
Gen <ssatou0320@gmail.com>
2025-06-02 06:33:11 +0000

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,19 @@
+//ナビゲーションバー
+
 export const Header: React.FC = () => {
   return (
-    <div className="py-6">
-      <div> Gen.com </div>
-      <div>
-        <a href="/">ホーム</a>
-        <a href="/">ポートフォリオ</a>
-        <a href="/">お客様</a>
-        <a href="/">
-          <button>お問い合わせ</button>
-        </a>
+    <header className="py-6">
+      <div className="container mx-auto flex justify-between items-center px-8 md:px-14">
+        <div className="text-3xl font-bold"> Gen.com </div>
+        <div className="space-x-12">
+          <a href="/">ホーム</a>
+          <a href="/">ポートフォリオ</a>
+          <a href="/">お客様</a>
+          <a href="/">
+            <button>お問い合わせ</button>
+          </a>
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
